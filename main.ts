209e23a9021987e650b063d324abf43e30996ee4
William Boily <william.boily@hotmail.ca>
2022-10-19 19:14:29 -0400

    fix time part of range when no date is provided.
    - display the time portion for the date range

--- main.ts
+++ main.ts
@@ -2,28 +2,40 @@ import 'dotenv/config'; // this will load the .env in process.env
 
 import ClickupClient from './ClickupClient';
 
-function printDayRange(start: Date, end: Date) {
-	const line = ''.concat(start.toLocaleDateString('UTC'))
-	  .concat(' to ')
-	  .concat(end.toLocaleDateString('UTC'));
-	console.log(line);
+function customDateTimeFormatter(date: Date): string {
+	const YMD = `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+	const time = customTimeFormatter(date);
+
+	return `${YMD}T${time}`;
 }
 
-function printTimeRange(start: Date, end: Date, taskName: string) {
-	// helper for format: https://devhints.io/wip/intl-datetime
+function customTimeFormatter(date: Date): string {
+	// other way to use JS Intl predefined format with option. https://devhints.io/wip/intl-datetime
 	let dateFormatter = new Intl.DateTimeFormat('UTC' , {
 		hour: '2-digit',
 		minute: '2-digit',
 		second: '2-digit',
-		hour12: false
+		hourCycle: 'h23' // to display 00 hour instead of 24
 	  });
+	
+	return dateFormatter.format(date);
+}
+
+function printDayRange(start: Date, end: Date) {
+	const line = ''.concat(customDateTimeFormatter(start))
+	  .concat(' to ')
+	  .concat(customDateTimeFormatter(end));
+	console.log(line);
+}
+
+function printTimeRange(start: Date, end: Date, taskName: string) {
 
 	const duration = (end.valueOf() - start.valueOf());
 
 	// equivalent : start.toLocaleTimeString('UTC', {hour12: false})
-	console.log(`+-- ${dateFormatter.format(start)}`);
+	console.log(`+-- ${customTimeFormatter(start)}`);
 	console.log('|' + ' '.repeat(13) + `${formatDuration(duration)} ` + taskName);
-	console.log(`+-- ${dateFormatter.format(end)}`);
+	console.log(`+-- ${customTimeFormatter(end)}`);
 }
 
 function formatDuration(durationInMs: number){
@@ -39,6 +51,16 @@ function printTotalTime(durationInMs: number) {
 	console.log(`total time: ${duration}`);
 }
 
+function stringDateYYYYMMDDtoDate(stringDate: string): Date {
+	const YMD = stringDate.split('-').map(el => {
+		return parseInt(el);
+	});
+
+	const date = new Date();
+	date.setFullYear(YMD[0], YMD[1]-1, YMD[2]);
+
+	return date;
+}
 
 (async () => {
 	try {
@@ -46,10 +68,11 @@ function printTotalTime(durationInMs: number) {
 		const myArgs = process.argv.slice(2);
 		const dateArgument = myArgs?.[0];
 
-		// when passing the time with the date, the local time zone is used.
-		// if only the date, UTC is used.
-		const startDate = dateArgument ? new Date(dateArgument.concat('T00:00:00')) : new Date();
-		const endDate = dateArgument ? new Date(dateArgument.concat('T23:59:59')) : new Date();
+		const startDate = dateArgument ? stringDateYYYYMMDDtoDate(dateArgument) : new Date();
+		startDate.setHours(0, 0, 0, 0);
+
+		const endDate = dateArgument ? stringDateYYYYMMDDtoDate(dateArgument) : new Date();
+		endDate.setHours(23, 59, 59, 999);
 
 		const clickup = new ClickupClient(process.env.CLICKUP_API_PERSONNAL_TOKEN ?? '');
 		const result = await clickup.getTimeEntriesWithinDateRange(process.env.CLICKUP_TEAM_ID ?? '', {start_date: startDate.valueOf(), end_date: endDate.valueOf()});
